c01ae8e6730bca049106988bb765ccff32d825ef
Muxammadamin Zoirov <muxammadaminzoirov@gmail.com>
2024-05-10 12:03:59 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,70 @@
+// 1-sovol
+// let summa = 5 % 2;
+// alert(5 + " % " + 2 + " = " + summa)
+
+// 2-sovol
+//  let taxminiyson = Math.floor(Math.random() * 10);
+//  alert(taxminiyson)
+
+// 3-sovol
+// let taxminiyson = Math.floor(Math.random() * 12.510);
+//  let a = '12';
+//  let b = Number(a);
+
+//  alert(b);
+
+// 4-sovol
+// let soz = ("Mars it school");
+
+// function harf() {
+// alert(soz.length);
+// }harf();
+
+// 5-sovol
+// let ismlar = 'Mars it school';
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(ismlar)
+// }
+
+// 6-sovol
+// let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+// let ism = harflar[11] + harflar[19] + harflar[21] + harflar[0] + harflar[11] + harflar[0] + harflar[2] + harflar[0] + harflar[11] + harflar[7] + harflar[11]
+// console.log(ism);
+
+// 7-sovol
+// let yosh = Number(prompt("Yoshingizni kiriting"));
+
+// if (yosh > 18) {
+//    alert("balovot yoshiga yotgansiz") 
+// } else {
+//     alert("balovat yoshiga yatmagansiz")
+// }
+
+// 8-sovol
+// let ism = prompt("Ismingizni kiriting")
+// let ismlar = ism.split('');
+// let teskari = ismlar.reverse().join('');
+// console.log(teskari);
+
+// 9-sovol
+// let sonlar = [2, 4, 9, 10, 45, 67, 8, 90];
+// let natija = [];
+
+// for (let i = 0; i < 8; i++) {   
+//     if (sonlar[i] % 2 === 0) {
+//         natija.push(sonlar[i])
+//     }
+// }
+
+// console.log(sonlar);
+// console.log(natija);
+
+// 10-sovol
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < sonlar.length; i++) {
+//   if (sonlar[i] % 2 === 0) {
+//     console.log(sonlar[i]);
+//   }
+// }
